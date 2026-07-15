8e968cfd8f891abe891fc9630926a997402596ee
rikuyama <126544184+rikuyama@users.noreply.github.com>
2026-07-15 12:54:58 +0900

    MiningFortuneListener.java を更新

--- src/main/java/me/lrg/skyblock/core/listener/MiningFortuneListener.java
+++ src/main/java/me/lrg/skyblock/core/listener/MiningFortuneListener.java
@@ -1,10 +1,10 @@
 package me.lrg.skyblock.core.listener;
 
 import me.lrg.skyblock.core.manager.FortuneManager;
-import me.lrg.skyblock.core.util.FortuneToolUtil;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.block.Block;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
@@ -16,7 +16,17 @@ import java.util.Collection;
 import java.util.Objects;
 
 /**
- * LRGMine管理外の通常採掘にMining Fortuneを適用するListener。
+ * Mining Fortuneを通常採掘ドロップへ反映するListener。
+ *
+ * このクラスの役割:
+ * - LRGMine範囲外の通常採掘にMining Fortuneを適用する
+ * - 幸運エンチャントを別倍率ではなく、Mining Fortune値の一部として扱う
+ *
+ * 注意:
+ * - LRGMine登録ブロックはLRGMine側のMineBreakListenerで処理する
+ * - event.setCancelled(true) は使わない
+ * - block.setType(AIR) は使わない
+ * - 作物には幸運エンチャントを乗せない
  */
 public class MiningFortuneListener implements Listener {
 
@@ -37,21 +47,25 @@ public class MiningFortuneListener implements Listener {
         }
 
         ItemStack originalTool = player.getInventory().getItemInMainHand();
-        ItemStack toolWithoutFortune = FortuneToolUtil.createWithoutVanillaFortune(originalTool);
+        ItemStack toolWithoutFortune = createToolWithoutFortune(originalTool);
+
         Collection<ItemStack> originalDrops = block.getDrops(toolWithoutFortune, player);
 
         if (originalDrops.isEmpty()) {
             return;
         }
 
-        double totalFortune = fortuneManager.getMiningFortune(player, blockType)
-                + FortuneToolUtil.getMiningFortuneFromEnchant(originalTool);
+        double totalFortune = 0.0;
+
+        totalFortune += fortuneManager.getMiningFortune(player, blockType);
+        totalFortune += getEnchantFortuneValue(originalTool);
 
         if (totalFortune <= 0.0) {
             return;
         }
 
         event.setDropItems(false);
+
         World world = block.getWorld();
 
         for (ItemStack originalDrop : originalDrops) {
@@ -66,6 +80,39 @@ public class MiningFortuneListener implements Listener {
         }
     }
 
+    private ItemStack createToolWithoutFortune(ItemStack originalTool) {
+        if (originalTool == null || originalTool.getType() == Material.AIR) {
+            return new ItemStack(Material.AIR);
+        }
+
+        ItemStack tool = originalTool.clone();
+        tool.removeEnchantment(Enchantment.FORTUNE);
+
+        return tool;
+    }
+
+    private double getEnchantFortuneValue(ItemStack tool) {
+        if (tool == null || tool.getType() == Material.AIR) {
+            return 0.0;
+        }
+
+        int level = tool.getEnchantmentLevel(Enchantment.FORTUNE);
+
+        return switch (level) {
+            case 1 -> 10.0;
+            case 2 -> 20.0;
+            case 3 -> 30.0;
+            case 4 -> 45.0;
+            default -> {
+                if (level <= 0) {
+                    yield 0.0;
+                }
+
+                yield 45.0 + ((level - 4) * 15.0);
+            }
+        };
+    }
+
     private boolean isMiningFortuneTarget(Material material) {
         return isOre(material)
                 || isBlockFortuneTarget(material)
@@ -77,82 +124,143 @@ public class MiningFortuneListener implements Listener {
         if (isOre(blockType)) {
             return isOreDrop(dropType);
         }
+
         if (isBlockFortuneTarget(blockType)) {
             return isBlockDrop(dropType);
         }
+
         if (isGemstone(blockType)) {
             return isGemstoneDrop(dropType);
         }
+
         if (isDwarvenMetal(blockType)) {
             return isDwarvenMetalDrop(dropType);
         }
+
         return false;
     }
 
     private boolean isOre(Material material) {
         return switch (material) {
-            case COAL_ORE, DEEPSLATE_COAL_ORE,
-                 IRON_ORE, DEEPSLATE_IRON_ORE,
-                 COPPER_ORE, DEEPSLATE_COPPER_ORE,
-                 GOLD_ORE, DEEPSLATE_GOLD_ORE,
-                 REDSTONE_ORE, DEEPSLATE_REDSTONE_ORE,
-                 EMERALD_ORE, DEEPSLATE_EMERALD_ORE,
-                 LAPIS_ORE, DEEPSLATE_LAPIS_ORE,
-                 DIAMOND_ORE, DEEPSLATE_DIAMOND_ORE,
-                 NETHER_GOLD_ORE, NETHER_QUARTZ_ORE -> true;
+            case COAL_ORE,
+                 DEEPSLATE_COAL_ORE,
+                 IRON_ORE,
+                 DEEPSLATE_IRON_ORE,
+                 COPPER_ORE,
+                 DEEPSLATE_COPPER_ORE,
+                 GOLD_ORE,
+                 DEEPSLATE_GOLD_ORE,
+                 REDSTONE_ORE,
+                 DEEPSLATE_REDSTONE_ORE,
+                 EMERALD_ORE,
+                 DEEPSLATE_EMERALD_ORE,
+                 LAPIS_ORE,
+                 DEEPSLATE_LAPIS_ORE,
+                 DIAMOND_ORE,
+                 DEEPSLATE_DIAMOND_ORE,
+                 NETHER_GOLD_ORE,
+                 NETHER_QUARTZ_ORE -> true;
             default -> false;
         };
     }
 
     private boolean isOreDrop(Material material) {
         return switch (material) {
-            case COAL, RAW_IRON, RAW_COPPER, RAW_GOLD,
-                 REDSTONE, EMERALD, LAPIS_LAZULI,
-                 DIAMOND, GOLD_NUGGET, QUARTZ -> true;
+            case COAL,
+                 RAW_IRON,
+                 RAW_COPPER,
+                 RAW_GOLD,
+                 REDSTONE,
+                 EMERALD,
+                 LAPIS_LAZULI,
+                 DIAMOND,
+                 GOLD_NUGGET,
+                 QUARTZ -> true;
             default -> false;
         };
     }
 
     private boolean isBlockFortuneTarget(Material material) {
         return switch (material) {
-            case STONE, COBBLESTONE, DEEPSLATE, COBBLED_DEEPSLATE,
-                 ANDESITE, DIORITE, GRANITE, TUFF, CALCITE,
-                 BASALT, BLACKSTONE, NETHERRACK, END_STONE -> true;
+            case STONE,
+                 COBBLESTONE,
+                 DEEPSLATE,
+                 COBBLED_DEEPSLATE,
+                 ANDESITE,
+                 DIORITE,
+                 GRANITE,
+                 TUFF,
+                 CALCITE,
+                 BASALT,
+                 BLACKSTONE,
+                 NETHERRACK,
+                 END_STONE -> true;
             default -> false;
         };
     }
 
     private boolean isBlockDrop(Material material) {
-        return isBlockFortuneTarget(material);
+        return switch (material) {
+            case STONE,
+                 COBBLESTONE,
+                 DEEPSLATE,
+                 COBBLED_DEEPSLATE,
+                 ANDESITE,
+                 DIORITE,
+                 GRANITE,
+                 TUFF,
+                 CALCITE,
+                 BASALT,
+                 BLACKSTONE,
+                 NETHERRACK,
+                 END_STONE -> true;
+            default -> false;
+        };
     }
 
     private boolean isGemstone(Material material) {
         return switch (material) {
-            case AMETHYST_BLOCK, BUDDING_AMETHYST, AMETHYST_CLUSTER,
-                 SMALL_AMETHYST_BUD, MEDIUM_AMETHYST_BUD,
+            case AMETHYST_BLOCK,
+                 BUDDING_AMETHYST,
+                 AMETHYST_CLUSTER,
+                 SMALL_AMETHYST_BUD,
+                 MEDIUM_AMETHYST_BUD,
                  LARGE_AMETHYST_BUD -> true;
             default -> false;
         };
     }
 
     private boolean isGemstoneDrop(Material material) {
-        return material == Material.AMETHYST_SHARD || material == Material.AMETHYST_BLOCK;
+        return switch (material) {
+            case AMETHYST_SHARD,
+                 AMETHYST_BLOCK -> true;
+            default -> false;
+        };
     }
 
     private boolean isDwarvenMetal(Material material) {
         return switch (material) {
-            case IRON_ORE, DEEPSLATE_IRON_ORE,
-                 GOLD_ORE, DEEPSLATE_GOLD_ORE,
-                 COPPER_ORE, DEEPSLATE_COPPER_ORE,
-                 RAW_IRON_BLOCK, RAW_GOLD_BLOCK, RAW_COPPER_BLOCK -> true;
+            case IRON_ORE,
+                 DEEPSLATE_IRON_ORE,
+                 GOLD_ORE,
+                 DEEPSLATE_GOLD_ORE,
+                 COPPER_ORE,
+                 DEEPSLATE_COPPER_ORE,
+                 RAW_IRON_BLOCK,
+                 RAW_GOLD_BLOCK,
+                 RAW_COPPER_BLOCK -> true;
             default -> false;
         };
     }
 
     private boolean isDwarvenMetalDrop(Material material) {
         return switch (material) {
-            case RAW_IRON, RAW_GOLD, RAW_COPPER,
-                 RAW_IRON_BLOCK, RAW_GOLD_BLOCK, RAW_COPPER_BLOCK -> true;
+            case RAW_IRON,
+                 RAW_GOLD,
+                 RAW_COPPER,
+                 RAW_IRON_BLOCK,
+                 RAW_GOLD_BLOCK,
+                 RAW_COPPER_BLOCK -> true;
             default -> false;
         };
     }
