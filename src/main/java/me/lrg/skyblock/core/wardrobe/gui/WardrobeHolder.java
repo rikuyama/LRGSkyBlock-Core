2fc25f8a5696f2e8e773ca31d5d13ceb3f14943f
rikuyama <126544184+rikuyama@users.noreply.github.com>
2026-07-19 20:42:16 +0900

    WardrobeHolder.java を作成

---
+++ src/main/java/me/lrg/skyblock/core/wardrobe/gui/WardrobeHolder.java
@@ -0,0 +1,11 @@
+package me.lrg.skyblock.core.wardrobe.gui;
+
+import org.bukkit.inventory.Inventory;
+import org.bukkit.inventory.InventoryHolder;
+
+public final class WardrobeHolder implements InventoryHolder {
+    @Override
+    public Inventory getInventory() {
+        return null;
+    }
+}
